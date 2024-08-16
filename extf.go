9cea3c2404ba8d85aa3e86074b1b3516cf943345
Titus T <dxtitus@gmail.com>
2024-08-16 03:58:07 +0000

    added mod

--- extf.go
+++ extf.go
@@ -1,45 +1,43 @@
 package extfilter
 
 import (
-	"strings"
 	"path/filepath"
+	"strings"
 )
 
 type ExtensionFilter struct {
 	filters []string
-	extmap  map[string]bool
+	dict    map[string]bool
 	strict  bool
 }
 
-//new extension filters
 func NewExtensionFilters(filters []string, strict bool) *ExtensionFilter {
-	ex := &ExtensionFilter{
+	var ext = &ExtensionFilter{
 		filters: filters,
 		strict:  strict,
 	}
-	return ex.updateFilterMap()
+	return ext.updateDictionary()
 }
 
-//Match on file name
-func (exf *ExtensionFilter) Match(fileName string) bool {
-	key := strings.TrimPrefix(filepath.Ext(fileName), ".")
-	return exf.extmap[key]
+// Match on file name
+func (ext *ExtensionFilter) Match(fileName string) bool {
+	var key = strings.TrimPrefix(filepath.Ext(fileName), ".")
+	return ext.dict[key]
 }
 
-//filter map
-func (exf *ExtensionFilter) updateFilterMap() *ExtensionFilter {
-	exf.extmap = make(map[string]bool)
-	for _, k := range exf.filters {
-		for _, key := range exf.genKeys(k) {
-			exf.extmap[key] = true
+func (ext *ExtensionFilter) updateDictionary() *ExtensionFilter {
+	ext.dict = make(map[string]bool, len(ext.filters)*3)
+	for _, k := range ext.filters {
+		for _, key := range ext.genKeys(k) {
+			ext.dict[key] = true
 		}
 	}
-	return exf
+	return ext
 }
 
-//generates filter keys
-func (exf *ExtensionFilter) genKeys(key string) []string {
-	if exf.strict {
+// generates filter keys
+func (ext *ExtensionFilter) genKeys(key string) []string {
+	if ext.strict {
 		return []string{key}
 	}
 	return []string{key, strings.ToUpper(key), strings.ToLower(key)}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/intdxdt/extfilter
+
+go 1.22
